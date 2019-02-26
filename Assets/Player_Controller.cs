aa22272a9b17116c6c8e6b2b9f618cfc3328497f
Andy Nguyen <anguyen5412@email.arizona.edu>
2019-02-26 01:42:23 -0700

    Added movement and changed a sprite

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    public Rigidbody2D player;
+    public float playerSpeed;
+    public float jumpPower;
+    ///private int health = 1;
+    float horizontal;
+    bool grounded;
+
+    void Start()
+    {
+        grounded = false;
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        player.velocity = new Vector2(horizontal * playerSpeed, player.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            player.AddForce(new Vector2(0f, jumpPower), ForceMode2D.Impulse);
+            grounded = false;
+        }
+
+        if (horizontal < 0)
+        {
+            transform.right = new Vector3(-1f, 0f, 0f);
+        }
+        else if (horizontal > 0)
+        {
+            transform.right = new Vector3(1f, 0f, 0f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Floor") && grounded == false)
+        {
+            grounded = true;
+        }
+    }
+}
