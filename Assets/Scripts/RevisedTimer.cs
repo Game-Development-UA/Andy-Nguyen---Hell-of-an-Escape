020aa0eb5d2e44caa78d5819f19f4d8b78d18c0c
Andy Nguyen <anguyen5412@email.arizona.edu>
2019-03-13 17:41:52 -0700

    Time Fix with Loss Condition + Level reset consistency

---
+++ Assets/Scripts/RevisedTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevisedTimer : MonoBehaviour
+{
+    [SerializeField] private GameObject DefeatUI;
+
+    public bool timesUp = false;
+    public Text timeText;
+    private float cur_time = 99;
+    private float elapsedTime;
+
+    void Start()
+    {
+        float elapsedTime = 0;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        timeText.text = ((cur_time - elapsedTime).ToString("TIME: 00"));
+
+        if (elapsedTime >= 99)
+        {
+            timesUp = true;
+            DefeatUI.SetActive(true);
+            elapsedTime = 0;
+        }
+
+    }
+}
